fab59aadae837f45f63d0e69ddb19f1a80806bed
Artem <aeposokhov@gmail.com>
2026-02-20 17:18:40 +0400

    sync

--- generate.py
+++ generate.py
@@ -1,20 +1,37 @@
-def main():
-    seed = input("enter seed key >> ")
+import random
 
-    def gen_part(seed):
-        part = (seed[0:3], seed[3:])
-        return part
 
-    part = gen_part(seed)
+def gen_part(seed):
+    part = (seed[0:3], seed[3:])
+    return part
 
-    def print_part(part):
-        return " ".join(part)
 
-    print(print_part(part))
+def keys(seed):
+    keys = []
 
-    for i in range(5):
+    for i in range(6):
+        keys.append(seed)
         seed = seed[-1] + seed[:-1]
-        print(print_part(gen_part(seed)))
+
+    return keys
 
 
-main()
+print("KEYLIST: AUTOMATIC")
+print("INFO: STARTING GEN")
+letters = list("abcdef")
+all_keys = []
+for i in range(4):
+    key = []
+    key.append(f"=day {i+1}=")
+    random.shuffle(letters)
+    keys_gen = keys("".join(letters))
+    for k in keys_gen:
+        key.append(f"{k[:3]} {k[3:]}")
+    all_keys.append(key)
+for (k1, k2), (k3, k4) in zip(
+    zip(all_keys[0], all_keys[1]), zip(all_keys[2], all_keys[3])
+):
+    print(f"{k1}    {k2}    {k3}    {k4}")
+print("use only your day's keys, just the 6")
+print("tomorrow use the next set of keys etc")
+print("so #1 today, then 2 -> 3 -> 4")
